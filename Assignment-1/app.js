bfddc65d6ec12fd905a9b627250f5052140da4e5
Fextur <galavishay.ga@gmail.com>
2025-01-19 01:07:05 +0200

    basic setup

---
+++ Assignment-1/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
