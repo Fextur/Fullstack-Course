cd11bb77fe72295688520e2f32e65bfd96ffdcd6
Fextur <galavishay.ga@gmail.com>
2025-01-19 01:36:31 +0200

    added getting all posts

--- Assignment-1/Controllers/postController.js
+++ Assignment-1/Controllers/postController.js
@@ -10,3 +10,12 @@ exports.createPost = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
