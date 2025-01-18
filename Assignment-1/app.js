d4ddb07d4684530afd5ff613a86cf291e8e46413
Fextur <galavishay.ga@gmail.com>
2025-01-19 01:19:02 +0200

    conneced mongo

--- Assignment-1/app.js
+++ Assignment-1/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const connectDB = require('./Config/db');
 
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
