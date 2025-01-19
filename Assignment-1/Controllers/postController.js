b3eb1b8cab1bfe7c6b15632227b9ca038412ef67
Fextur <galavishay.ga@gmail.com>
2025-01-19 02:13:00 +0200

    added update put logic

--- Assignment-1/Controllers/postController.js
+++ Assignment-1/Controllers/postController.js
@@ -42,3 +42,22 @@ exports.getPostsBySender = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.updatePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content, sender } = req.body;
+
+        const post = await Post.findByIdAndUpdate(
+            id,
+            { title, content, sender },
+            { new: true, runValidators: true } 
+        );
+
+        if (!post) return res.status(404).json({ error: 'Post not found' });
+
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
