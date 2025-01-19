7e07982a472a40b767a9f5e9a244fe141ab76457
Fextur <galavishay.ga@gmail.com>
2025-01-19 02:35:55 +0200

    added other crud opertaions for comment

--- Assignment-1/Controllers/commentController.js
+++ Assignment-1/Controllers/commentController.js
@@ -12,3 +12,50 @@ exports.createComment = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.getAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.status(200).json(comments);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.updateComment = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { content, sender } = req.body;
+
+        const comment = await Comment.findByIdAndUpdate(
+            id,
+            { content, sender },
+            { new: true, runValidators: true }
+        );
+
+        if (!comment) {
+            return res.status(404).json({ error: 'Comment not found' });
+        }
+
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.deleteComment = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const comment = await Comment.findByIdAndDelete(id);
+
+        if (!comment) {
+            return res.status(404).json({ error: 'Comment not found' });
+        }
+
+        res.status(200).json({ message: 'Comment deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
--- Assignment-1/Routes/commentRoutes.js
+++ Assignment-1/Routes/commentRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const commentController = require('../Controllers/commentController');
 
 router.post('/', commentController.createComment);
+router.get('/', commentController.getAllComments);
+router.put('/:id', commentController.updateComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
